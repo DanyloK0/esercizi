a5ba10e2f561a55fa861fad8c5da9279030057ab
danylo <dany071298@gmail.com>
2021-05-04 11:35:36 +0200

    add counters

---
+++ src/Counter/style.jsx
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const MainContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  color: red;
+`;
+export const InputContainer = styled.div``;
